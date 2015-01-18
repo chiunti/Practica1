b93d22ce4f13a3aefcf264ba98350a01163b1ca3
Chiunti <chiunti.abc@gmail.com>
2015-01-17 19:50:53 -0600

    Initial Commit

---
+++ Practica1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Practica1
+//
+//  Created by Chiunti on 17/01/15.
+//  Copyright (c) 2015 Chiunti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
